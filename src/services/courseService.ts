69430d88da1fd901bc5d31fa4a4a43ee1b970d24
joaomidowz <jgcustodio2005@gmail.com>
2023-11-23 09:47:36 -0300

    Obtendo 3 cursos em destaque

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -10,7 +10,7 @@ export const courseService = {
             ['thumbnail_url', 'thumbailUrl']
         ],
         include: {
-            association: "Episodes",
+            association: "episodes",
             attributes: [
                 "id",
                 "name",
@@ -25,5 +25,24 @@ export const courseService = {
         })
 
         return courseWithEpisodes
+    },
+
+    getRandomFeaturedCourses: async () => {
+        const featuredCourses = await Course.findAll({
+            attributes: [
+                "id",
+                "name",
+                "synopsis",
+                ['thumbnail_url', 'thumbailUrl']
+            ],
+            where: {
+                featured: true,
+            }
+        })
+
+        const randomFeaturedCourses = featuredCourses.sort(() => 0.5 - Math.random())
+        
+        return randomFeaturedCourses.slice(0, 3)
     }
+
 }
