b00ccf8cd2030d8f84facbea92971309e2fa4cab
joaomidowz <jgcustodio2005@gmail.com>
2023-11-29 07:35:41 -0300

    Atualizando informações do usuário

--- src/routes.ts
+++ src/routes.ts
@@ -35,6 +35,7 @@ router.post('/likes', ensureAuth, likesController.save)
 router.delete('/likes/:id', ensureAuth, likesController.delete)
 
 router.get('/users/current/watching', ensureAuth, usersController.wathching)
+router.put('/users/current', ensureAuth, usersController.update)
 router.get('/users/current', ensureAuth, usersController.show)
 
 export { router }
