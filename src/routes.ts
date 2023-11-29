e135e9c805f51339bbe1ee616cb3b5809c0472ac
joaomidowz <jgcustodio2005@gmail.com>
2023-11-29 07:55:47 -0300

    Atualizando a senha

--- src/routes.ts
+++ src/routes.ts
@@ -36,6 +36,7 @@ router.delete('/likes/:id', ensureAuth, likesController.delete)
 
 router.get('/users/current/watching', ensureAuth, usersController.wathching)
 router.put('/users/current', ensureAuth, usersController.update)
+router.put('/users/current/password', ensureAuth, usersController.updatePassword)
 router.get('/users/current', ensureAuth, usersController.show)
 
 export { router }
